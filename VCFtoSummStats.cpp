cbcc40cc664c806656ddf1f217d469d209cab24f
flaxmans <sam.flaxman@gmail.com>
2019-05-28 18:13:42 -0600

    First commit

---
+++ VCFtoSummStats.cpp
@@ -0,0 +1,17 @@
+// program for generating summary stats such as FST and DXY
+// from a VCF
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    // start writing pseudocode for main operations here:
+    
+    
+    
+    return 0;
+}
+
+
