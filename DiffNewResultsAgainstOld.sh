772bd66c9d8c43f31bc01e784151645b37db0610
flaxmans <sam.flaxman@gmail.com>
2019-06-12 17:23:04 -0600

    made a new script for comparing new results with old

---
+++ DiffNewResultsAgainstOld.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+cd ExampleDataFiles/
+
+testList="Small_hmel2.5.30f4.vcf Medium_hmel2.5.30f4.vcf OneHundredK_TotChromCloeNolan.vcf"
+
+endings1="_Unfiltered_Summary.tsv .bz2_Unfiltered_Summary.tsv .gz_Unfiltered_Summary.tsv"
+
+endings2="_discardedLineNums.txt .bz2_discardedLineNums.txt .gz_discardedLineNums.txt"
+
+for i in $testList
+do
+	ref="ref_${i}_Unfiltered_Summary.tsv"
+	for j in $endings1
+	do
+		testFile="${i}${j}"
+		if [ -f "$testFile" ]
+		then
+			cmd="diff $ref $testFile"
+			echo "$cmd :"
+			$cmd
+			printf "\n\t*******************\n"
+		fi
+	done
+	ref="ref_${i}_discardedLineNums.txt"
+	for j in $endings2
+	do
+		testFile="${i}${j}"
+		if [ -f "$testFile" ]
+		then
+			cmd="diff $ref $testFile"
+			echo "$cmd :"
+			$cmd
+			printf "\n\t*******************\n"
+		fi
+	done
+done
+
+
+cd ..
