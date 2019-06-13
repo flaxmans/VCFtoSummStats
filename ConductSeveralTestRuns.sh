25a63629ea8ba527206de342445034046124f28e
Samuel Flaxman <flaxmans@insilico.colorado.edu>
2019-06-13 12:18:43 -0600

    updating test scripts

--- ConductSeveralTestRuns.sh
+++ ConductSeveralTestRuns.sh
@@ -2,17 +2,20 @@
 
 numTests=6
 
-fileList=(BASW_100k.vcf.gz Medium_hmel2.5.30f4.vcf.gz OneHundredK_TotChromCloeNolan.vcf.gz RADseq_allSamples.vcf.gz hmel2.5.30f4.vcf.gz)
+fileList=(BASW_100k.vcf.gz Medium_hmel2.5.30f4.vcf.gz OneHundredK_TotChromCloeNolan.vcf.gz RADseq_allSamples.vcf.gz hmel2.5.30f4.vcf.gz Total_Chromosomes_Combined_with_header.vcf.gz)
 
 # following order must correspond to fileList VCFs!
-popFiles=(BASW_popMap.txt popFileHmel.txt CloeNolanPopData.txt BASW_popMap.txt popFileHmel.txt)
+popFiles=(BASW_popMap.txt popFileHmel.txt CloeNolanPopData.txt BASW_popMap.txt popFileHmel.txt CloeNolanPopData.txt)
 
-for ((i=1; i < 3; i++))
+for ((i=0; i < $numTests; i++))
 do
     vf=${fileList[$i]}
-    pf=${popFiles[$i]}
-    cmd="./VCFtoSummStats -V ExampleDataFiles/${vf} -P ExampleDataFiles/${pf}"
-    echo "$cmd"
-    $cmd
-    printf "\nDone with test $i\n\n************\n"
+    if [ -f "$vf" ]
+    then
+      pf=${popFiles[$i]}
+      cmd="./VCFtoSummStats -V ExampleDataFiles/${vf} -P ExampleDataFiles/${pf}"
+      echo "$cmd"
+      $cmd
+      printf "\nDone with test $i\n\n************\n\n"
+    fi
 done
